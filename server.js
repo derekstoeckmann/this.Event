0f3a413400ba130ffffead598cc9808d7ef7aaed
Derek Stoeckmann <d.stoeckmann@gmail.com>
2019-12-27 16:02:02 -0700

    add express router and initial events route file

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 
-app.get("/", (req, res) => res.send("Hello world!"));
+// API Route files
+const events = require("./routes/events");
+
+// API Routes
+app.use("/api/events", events);
+
+// This will eventually be the React entrance route
+app.get("*", (req, res) => res.send("Hello world!"));
 
 app.listen(PORT, () =>
   console.log(`Express server listening on port ${PORT}.`)

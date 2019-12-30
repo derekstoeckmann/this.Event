d6488653f322e64c30bdc8e5acd6aba8ccf6ccf9
Derek Stoeckmann <d.stoeckmann@gmail.com>
2019-12-30 08:52:07 -0700

    add API routes and controller methods for /api/users and /api/users/:id

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const PORT = 3001;
 const connectToDatabase = require("./config/db");
 const events = require("./routes/events");
+const users = require("./routes/users");
 
 // Middleware
 app.use(express.json());
 
 // Router
 app.use("/api/events", events);
+app.use("/api/users", users);
 
 // This will eventually be the React entrance route
 app.get("*", (req, res) => res.send("Hello world!"));

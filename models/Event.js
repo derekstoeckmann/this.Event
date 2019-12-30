4cef65759a8c2a7388864c48be216a163a881c6a
Derek Stoeckmann <d.stoeckmann@gmail.com>
2019-12-30 08:08:46 -0700

    add timestamps to event schema

--- models/Event.js
+++ models/Event.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
 
-const EventSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Please add a title."],
-    trim: true
+const EventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Please add a title."],
+      trim: true
+    },
+    description: {
+      type: String,
+      required: [true, "Please add a description."]
+    },
+    public: Boolean,
+    tags: [String]
   },
-  description: {
-    type: String,
-    required: [true, "Please add a description."]
-  },
-  public: Boolean,
-  tags: [String]
-});
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model("Event", EventSchema);

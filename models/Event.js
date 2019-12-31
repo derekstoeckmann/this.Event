1ab6591901b9f10abf573fcc6deacdec1eb12f6f
Derek Stoeckmann <d.stoeckmann@gmail.com>
2019-12-31 07:01:03 -0700

    add bringYourOwn and highlights fields to event schema, as well as additional validation

--- models/Event.js
+++ models/Event.js
@@ -5,45 +5,69 @@ const EventSchema = new mongoose.Schema(
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
-      required: true
+      required: [true, "Event must have a creator."]
     },
     title: {
       type: String,
-      required: [true, "Please add a title."],
+      required: [true, "Event must have a title."],
       trim: true
     },
     description: {
       type: String,
-      required: [true, "Please add a description."]
+      required: [true, "Event must have a description."]
     },
     time: {
       type: Date,
-      required: true
+      required: [true, "Event must have a date and time."]
     },
     streetAddress: {
       type: String,
-      required: true
+      required: [true, "Event must have an address."]
     },
     city: {
       type: String,
-      required: true
+      required: [true, "Event must have a city."]
     },
     state: {
       type: String,
-      required: true
+      required: [true, "Event must have a state."]
     },
     zip: {
       type: String,
-      required: true
+      required: [true, "Event must have a zip code."]
     },
-    attending: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-      }
-    ],
-    public: Boolean,
-    tags: [String],
+    attending: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+        }
+      ]
+    },
+    bringYourOwn: {
+      type: String,
+      trim: true
+    },
+    highlights: {
+      type: [String],
+      validate: [
+        array => array.length <= 5,
+        "No more than 5 highlights allowed."
+      ]
+    },
+    public: {
+      type: Boolean,
+      default: true
+    },
+    tags: {
+      type: [
+        {
+          type: String,
+          trim: true,
+          lowercase: true
+        }
+      ]
+    },
     category: {
       type: String,
       required: true,

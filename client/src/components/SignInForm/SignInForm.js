796c135ba1a32855fa3a48938874161054cd8605
Aaron Angle <acangle10@gmail.com>
2020-01-07 14:06:22 -0700

    sign up form

--- client/src/components/SignInForm/SignInForm.js
+++ client/src/components/SignInForm/SignInForm.js
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 
 const SignInForm = () => {
+    const [userInfo, getUserInfo] = useState({
+        username: "",
+        id: ""
+    });
 
+    function handleChange() {
+        getUserInfo({
+            [event.target.value]: event.target.name
+        })
+    }
     return (
         <form>
             <label>Username</label>
-            <input type="text"></input>
+            <input type="text" name="username" placeholder="Username" value={userInfo.username} onChange={handleChange}></input>
             <label>Password</label>
-            <input type="password"></input>
-            <button>Sign In</button>
+            <input type="password" name="password" placeholder="Password" value={userInfo.password} onChange={handleChange}></input>
+            <button>Submit</button>
         </form>
     )
 }
--- client/src/components/SignUpForm/SignUpForm.js
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Auth } from "aws-amplify";
+
 
 const SignUpForm = () => {
     const [userInfo, getUserInfo] = useState({
@@ -7,25 +9,43 @@ const SignUpForm = () => {
         username: "",
         email: "",
         password: "",
-        id: ""
-    })
+        phone_number: "",
+        id: "",
+        signedup: false
+    });
 
     function handleChange() {
-
+        getUserInfo({
+            [event.target.value]: event.target.name
+        })
     }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        Auth.signUp({
+            username: userInfo.username,
+            password: userInfo.password,
+            attributes: {
+                email: userInfo.email,
+                phone_number: userInfo.phone_number
+            }
+        })
+            .then()
+    }
+
     return (
         <form>
             <label>First Name</label>
-            <input type="text" value={userInfo.firstName}></input>
+            <input type="text" name="firstname" placeholder="First Name" value={userInfo.firstName} onChange={handleChange}></input>
             <label>Last Name</label>
-            <input type="text" value={userInfo.lastName}></input>
+            <input type="text" name="lastname" placeholder="Last Name" value={userInfo.lastName} onChange={handleChange}></input>
             <label>Username</label>
-            <input type="text" value={userInfo.username}></input>
+            <input type="text" name="username" placeholder="Username" value={userInfo.username} onChange={handleChange}></input>
             <label>Email</label>
-            <input type="text" value={userInfo.email}></input>
+            <input type="text" name="email" placeholder="Email" value={userInfo.email} onChange={handleChange}></input>
             <label>Password</label>
-            <input type="password" value={userInfo.password}></input>
-            <button>Submit</button>
+            <input type="password" name="password" placeholder="Password" value={userInfo.password} onChange={handleChange}></input>
+            <button onClick={() => handleSubmit}>Submit</button>
         </form>
     )
 }

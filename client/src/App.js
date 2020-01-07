c0a408644650486462bd105eebea1d4ddcee33bf
Aaron Angle <acangle10@gmail.com>
2020-01-07 10:09:50 -0700

    render home page if logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Amplify from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import aws_exports from "./aws-exports";
 import Home from "./pages/Home/Home";
 import CreateEvent from "./pages/CreateEvent/CreateEvent";
@@ -10,16 +10,45 @@ import LandingPage from "./pages/LandingPage/LandingPage";
 Amplify.configure(aws_exports);
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/createEvent" component={CreateEvent} />
-        <Route exact path="/event" component={Event} />
-      </Switch>
-    </Router>
-  );
+
+  const [currentUser, getCurrentUser] = useState();
+
+  Auth.currentUserInfo()
+    .then(res => {
+      if (res !== null) {
+        getCurrentUser(res.username);
+      } else {
+        getCurrentUser(null);
+      }
+    })
+    .catch(err => {
+      console.log("error", err)
+    })
+
+
+  if (currentUser) {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/createEvent" component={CreateEvent} />
+          <Route exact path="/event" component={Event} />
+          <Route component={Home} />
+        </Switch>
+      </Router>
+    )
+  } else {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/createEvent" component={CreateEvent} />
+          <Route exact path="/event" component={Event} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;

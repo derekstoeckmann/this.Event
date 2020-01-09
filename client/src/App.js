dfb97e2a5e38caab1de0ddecaea5c2c776295036
Derek Stoeckmann <d.stoeckmann@gmail.com>
2020-01-08 17:12:15 -0700

    fetch user data from mongodb based on auth email and add user data to global context

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Amplify, { Auth } from "aws-amplify";
+import axios from "axios";
 import aws_exports from "./aws-exports";
 import Home from "./pages/Home/Home";
 import Search from "./pages/Search/Search";
 import CreateEvent from "./pages/CreateEvent/CreateEvent";
 import Event from "./pages/Event/Event";
 import LandingPage from "./pages/LandingPage/LandingPage";
-import SignUpForm from './components/SignUpForm/SignUpForm';
-import SignInForm from './components/SignInForm/SignInForm';
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignInForm from "./components/SignInForm/SignInForm";
 import IsLoggedIn from "./utils/IsLoggedIn";
 import CurrentUserEmail from "./utils/CurrentUserEmail";
 
 Amplify.configure(aws_exports);
 
 function App() {
-
   const [currentUser, getCurrentUser] = useState("");
+  const [currentUserData, setCurrentUserData] = useState();
   const [loggedIn, getLoggedIn] = useState("");
 
-  const isLoggedIn = (value) => {
-    getLoggedIn(value)
-  }
+  const isLoggedIn = value => {
+    getLoggedIn(value);
+  };
 
   useEffect(() => {
     Auth.currentUserInfo()
       .then(res => {
         if (res !== null) {
           getCurrentUser(res.attributes.email);
+
+          axios
+            .get(`/api/users?email=${currentUser}`)
+            .then(response => {
+              setCurrentUserData({ ...response.data.data[0] });
+            })
+            .catch(err => console.log(err));
         } else {
           getCurrentUser(null);
         }
       })
       .catch(err => {
-        console.log("error", err)
-      })
-  }, [loggedIn])
+        console.log("error", err);
+      });
+  }, [loggedIn]);
 
   if (loggedIn || currentUser) {
     return (
-      <CurrentUserEmail.Provider value={{ currentUser }}>
+      <CurrentUserEmail.Provider value={{ currentUserData }}>
         <IsLoggedIn.Provider value={{ loggedIn, isLoggedIn }}>
           <Router>
             <Switch>
@@ -52,17 +60,25 @@ function App() {
           </Router>
         </IsLoggedIn.Provider>
       </CurrentUserEmail.Provider>
-    )
+    );
   } else {
     return (
       <Router>
         <Switch>
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/login" render={(props) => <SignInForm {...props} value={isLoggedIn} />} />
-          <Route exact path="/signup" render={(props) => <SignUpForm {...props} value={isLoggedIn} />} />
+          <Route
+            exact
+            path="/login"
+            render={props => <SignInForm {...props} value={isLoggedIn} />}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={props => <SignUpForm {...props} value={isLoggedIn} />}
+          />
         </Switch>
       </Router>
-    )
+    );
   }
 }
 

ec6ba091361ea1c5e9d9740034a848036f3b06a1
Chris Neal <voodoodaddy2000@gmail.com>
2020-01-08 17:00:11 -0700

    Partial data population on Event Detail Page

--- client/src/components/SingleEvent/SingleEvent.js
+++ client/src/components/SingleEvent/SingleEvent.js
@@ -1,40 +1,43 @@
 import React from "react";
 import Moment from "react-moment";
+import { Link } from "react-router-dom";
 
 import Grid from "@material-ui/core/Grid";
 
 import styles from "./SingleEvent.module.css";
 
 function SingleEvent(props) {
-  const { time, title, shortDescription } = props;
+  const { _id, time, title, shortDescription } = props;
   return (
     <>
-      <Grid item xs={12} className={styles["main-events-white"]}>
-        <Grid container direction="row">
-          <Grid
-            item
-            xs={2}
-            style={{ borderRight: "1px solid #ccc", padding: "3px" }}
-          >
-            <Moment date={time} format="hh:mm a" />
-            <br />
-          </Grid>
-          <Grid item xs={10}>
-            <Grid container direction="row">
-              <Grid
-                item
-                xs={12}
-                style={{ borderBottom: "1px solid #ccc", padding: "5px" }}
-              >
-                {title}
-              </Grid>
-              <Grid item xs={12} style={{ padding: "5px" }}>
-                {shortDescription}
+      <Link to={`/event/${_id}`}>
+        <Grid item xs={12} className={styles["main-events-white"]}>
+          <Grid container direction="row">
+            <Grid
+              item
+              xs={2}
+              style={{ borderRight: "1px solid #ccc", padding: "3px" }}
+            >
+              <Moment date={time} format="hh:mm a" />
+              <br />
+            </Grid>
+            <Grid item xs={10}>
+              <Grid container direction="row">
+                <Grid
+                  item
+                  xs={12}
+                  style={{ borderBottom: "1px solid #ccc", padding: "5px" }}
+                >
+                  {title}
+                </Grid>
+                <Grid item xs={12} style={{ padding: "5px" }}>
+                  {shortDescription}
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </Link>
       <br />
     </>
   );

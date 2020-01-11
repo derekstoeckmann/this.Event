c0c8a1aeabddecf80098722eb995a004ea309bdb
Chris Neal <voodoodaddy2000@gmail.com>
2020-01-11 00:53:14 -0700

    Redesigned Login with material

--- client/src/components/SignInForm/SignInForm.js
+++ client/src/components/SignInForm/SignInForm.js
@@ -1,56 +1,118 @@
 import React, { useState } from "react";
 import { Auth } from "aws-amplify";
 import ErrorBanner from "../ErrorBanner/ErrorBanner";
-import styles from "../SignUpForm/SignUpForm.module.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+import styles from "./SignInForm.module.css";
+
+import { Grid, Button, Container, TextField } from "@material-ui/core";
 
 const SignInForm = (props) => {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-    const isLoggedIn = props.value;
-    const isSignedIn = props.signed
+  const isLoggedIn = props.value;
+  const isSignedIn = props.signed
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        setLoading(true)
-        Auth.signIn({
-            username: username,
-            password: password,
-        })
-            .then(res => {
-                isLoggedIn(true)
-                isSignedIn(true)
-                props.history.push("/")
-            })
-            .catch(err => {
-                setLoading(false)
-                setError(err.message)
-            })
-    }
-    return (
-        <div>
-            <div className={styles.form}>
-                <ErrorBanner>
-                    {error}
-                </ErrorBanner>
-                <h1 className={styles.title}>Sign In</h1>
-                <form className={styles.formInput}>
-                    <label>Username</label>
-                    <input className={styles.formInputBox} type="text" name="username" placeholder="Username" value={username} onChange={e => setUsername(e.target.value)}></input>
-                    <label>Password</label>
-                    <input className={styles.formInputBox} type="password" name="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}></input>
-                    <p>Don't Have an Account Yet? <Link to={"/signup"}>Create Account</Link></p>
-                    <button className={styles.btn} onClick={handleSubmit} disabled={loading}>
-                        {loading && <img className={styles.loading} alt="submit button" src={require("./refresh.png")} />}
-                        {!loading && "Submit"}
-                    </button>
-                </form>
-            </div>
-        </div>
-    )
+  function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true)
+    Auth.signIn({
+      username: username,
+      password: password,
+    })
+      .then(res => {
+        isLoggedIn(true)
+        isSignedIn(true)
+        props.history.push("/")
+      })
+      .catch(err => {
+        setLoading(false)
+        setError(err.message)
+      })
+  }
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <Container maxWidth="lg" className={styles["main-top"]}>
+        <form>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            alignContent="center"
+            className={styles["main-top-inner"]}
+          >
+            <Grid item xs={12} className={styles["center"]}>
+              <ErrorBanner>
+                {error}
+              </ErrorBanner>
+            </Grid>
+            <Grid item xs={12} className={styles["center"]}>
+              <span className={styles["small-letters"] + " " + styles["shadow"]}>this.</span><span className={styles["large-letters"] + " " + styles["shadow"]}>E</span><span
+                className={styles["small-letters"] + " " + styles["shadow"]}>vent</span><br />
+            </Grid>
+            <Grid item xs={12} className={styles["center"]}>
+              <h1 className={styles["white"] + " " + styles["shadow"]}>Sign In</h1>
+            </Grid>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              alignContent="center"
+              className={styles["textfield-wrapper"]}
+            >
+              <Grid item xs={11} className={styles["center"]}>
+                <TextField
+                  name="username"
+                  id="username"
+                  label="Username"
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
+                  variant="outlined"
+                  color="primary"
+                  size="small"
+                  className={styles["data-value-input"]}
+                />
+              </Grid>
+              <Grid item xs={11} className={styles["center"]}>
+                <br />
+              </Grid>
+              <Grid item xs={11} className={styles["center"]}>
+                <TextField
+                  name="password"
+                  type="password"
+                  id="password"
+                  label="Password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  variant="outlined"
+                  color="primary"
+                  size="small"
+                  className={styles["data-value-input"]}
+                />
+              </Grid>
+            </Grid>
+            <Grid item xs={11} className={styles["center"]}>
+              <p className={styles["blue"]}>Don't Have an Account Yet? <Link to={"/signup"}>Create Account</Link></p>
+            </Grid>
+            <Grid item xs={11} className={styles["center"]}>
+              <Button variant="contained" color="primary" onClick={handleSubmit} disabled={loading}>
+                {loading && <img className={styles.loading} alt="submit button" src={require("./refresh.png")} />}
+                {!loading && "Submit"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
+    </div >
+  )
 }
 
 export default SignInForm;
--- client/src/components/SignUpForm/SignUpForm.js
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -6,96 +6,96 @@ import styles from "./SignUpForm.module.css";
 import { Link } from "react-router-dom";
 
 const SignUpForm = (props) => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmationCode, setConfirmationCode] = useState("");
-    const [signedup, setSignedUp] = useState("");
-    const [error, setError] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmationCode, setConfirmationCode] = useState("");
+  const [signedup, setSignedUp] = useState("");
+  const [error, setError] = useState("");
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        if (!signedup) {
-            Auth.signUp({
-                username: email,
-                password: password,
-                attributes: {
-                    phone_number: "+11234561234"
-                }
-            })
-                .then(res => {
-                    setError("")
-                    setSignedUp(true)
-                })
-                .catch(err => {
-                    setError(err.message)
-                })
-        } else {
-            Auth.confirmSignUp(email, confirmationCode)
-                .then((res) => {
-                    const userData = {
-                        firstName,
-                        lastName,
-                        email
-                    }
-                    axios.post("/api/users", userData)
-                        .then(res => {
-                            props.history.push("/login")
-                        })
-                        .catch(err => {
-                            console.log("axios Error")
-                            console.log(err)
-                            setError("Email May Already Be Registered In Database")
-                        })
-                })
-                .catch(err => {
-                    setError(err.message)
-                })
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!signedup) {
+      Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          phone_number: "+11234561234"
         }
-    }
-    if (signedup) {
-        return (
-            <div>
-                <div className={styles.form} >
-                    <ErrorBanner >
-                        {error}
-                    </ErrorBanner>
-                    <h1 className={styles.title}>Confirm Your Account With Email</h1>
-                    <form className={styles.formInput}>
-                        <label>Email</label>
-                        <input className={styles.formInputBox} type="text" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}></input>
-                        <label>Confirmation Email Code</label>
-                        <input className={styles.formInputBox} type="text" name="confirmationCode" value={confirmationCode} onChange={e => setConfirmationCode(e.target.value)}></input>
-                        <button className={styles.btn} onClick={handleSubmit}>Submit</button>
-                    </form>
-                </div >
-            </div>
-        )
+      })
+        .then(res => {
+          setError("")
+          setSignedUp(true)
+        })
+        .catch(err => {
+          setError(err.message)
+        })
     } else {
-        return (
-            <div>
-                <div className={styles.form}>
-                    <ErrorBanner>
-                        {error}
-                    </ErrorBanner>
-                    <h1 className={styles.title}>Sign Up</h1>
-                    <form className={styles.formInput}>
-                        <label>First Name</label>
-                        <input className={styles.formInputBox} type="text" name="firstname" placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)}></input>
-                        <label>Last Name</label>
-                        <input className={styles.formInputBox} type="text" name="lastname" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)}></input>
-                        <label>Email</label>
-                        <input className={styles.formInputBox} type="text" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}></input>
-                        <label>Password</label>
-                        <input className={styles.formInputBox} type="password" name="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}></input>
-                        <p>Already Have an Account? <Link to={"/login"}>Sign In</Link></p>
-                        <button className={styles.btn} onClick={handleSubmit}>Submit</button>
-                    </form>
-                </div>
-            </div>
-        )
+      Auth.confirmSignUp(email, confirmationCode)
+        .then((res) => {
+          const userData = {
+            firstName,
+            lastName,
+            email
+          }
+          axios.post("/api/users", userData)
+            .then(res => {
+              props.history.push("/login")
+            })
+            .catch(err => {
+              console.log("axios Error")
+              console.log(err)
+              setError("Email May Already Be Registered In Database")
+            })
+        })
+        .catch(err => {
+          setError(err.message)
+        })
     }
+  }
+  if (signedup) {
+    return (
+      <div>
+        <div className={styles.form} >
+          <ErrorBanner >
+            {error}
+          </ErrorBanner>
+          <h1 className={styles.title}>Confirm Your Account With Email</h1>
+          <form className={styles.formInput}>
+            <label>Email</label>
+            <input className={styles.formInputBox} type="text" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}></input>
+            <label>Confirmation Email Code</label>
+            <input className={styles.formInputBox} type="text" name="confirmationCode" value={confirmationCode} onChange={e => setConfirmationCode(e.target.value)}></input>
+            <button className={styles.btn} onClick={handleSubmit}>Submit</button>
+          </form>
+        </div >
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <div className={styles.form}>
+          <ErrorBanner>
+            {error}
+          </ErrorBanner>
+          <h1 className={styles.title}>Sign Up</h1>
+          <form className={styles.formInput}>
+            <label>First Name</label>
+            <input className={styles.formInputBox} type="text" name="firstname" placeholder="First Name" value={firstName} onChange={e => setFirstName(e.target.value)}></input>
+            <label>Last Name</label>
+            <input className={styles.formInputBox} type="text" name="lastname" placeholder="Last Name" value={lastName} onChange={e => setLastName(e.target.value)}></input>
+            <label>Email</label>
+            <input className={styles.formInputBox} type="text" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)}></input>
+            <label>Password</label>
+            <input className={styles.formInputBox} type="password" name="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)}></input>
+            <p>Already Have an Account? <Link to={"/login"}>Sign In</Link></p>
+            <button className={styles.btn} onClick={handleSubmit}>Submit</button>
+          </form>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default SignUpForm;
